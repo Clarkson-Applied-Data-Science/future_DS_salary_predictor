9a31b12d54e4963f6cef2fd12860d6c3e3b557c2
EDEN ABDISA <edenabdisa90@gmail.com>
2025-04-23 13:14:52 -0400

    Cleaned up and added more tests

--- LinearRegression/Model.R
+++ LinearRegression/Model.R
@@ -13,3 +13,41 @@ salary_data$predicted_salary <- predict(model, newdata = salary_data_encoded)
 # Plot model diagnostics
 par(mfrow = c(2, 2))
 plot(model)
+
+# Apply one-hot encoding to the dataset
+test_salary_data_one_hot <- predict(dummy, newdata = test_data)
+test_salary_data_one_hot <- data.frame(test_salary_data_one_hot)
+test_salary_data_combined <- cbind(test_data, test_salary_data_one_hot)
+test_data <- test_salary_data_combined[, !names(test_salary_data_combined) %in% c("work_year", "experience_level", "employment_type",  "remote_ratio", "company_size","job_category")]
+
+test_data <- test_data %>%
+  mutate(salary_in_usd = (salary_in_usd - min_salary) / (max_salary - min_salary))
+
+
+
+
+#Test the model
+
+test_data$predicted_salary <- predict(model, newdata = test_data)
+
+test_data$salary_in_usd <- (test_data$salary_in_usd * (max_salary - min_salary)) + min_salary
+test_data$predicted_salary_original <- (test_data$predicted_salary * (max_salary - min_salary)) + min_salary
+head(test_data[, c("job_title", "salary_in_usd", "predicted_salary_original")])
+
+
+
+# Calculate RMSE and MAE
+rmse_value <- rmse(test_data$salary_in_usd, test_data$predicted_salary_original)
+mae_value <- mae(test_data$salary_in_usd, test_data$predicted_salary_original)
+#r_squared <- rsq(test_data$salary_in_usd, test_data$predicted_salary_original)
+
+rss <- sum((test_data$salary_in_usd - test_data$predicted_salary_original)^2)  # Residual Sum of Squares
+tss <- sum((test_data$salary_in_usd - mean(test_data$salary_in_usd))^2)        # Total Sum of Squares
+
+r_squared <- 1 - rss/tss
+print(r_squared)
+
+# Print RMSE and MAE
+print(paste("RMSE:", rmse_value))
+print(paste("MAE:", mae_value))
+#print(paste("R2:", r_squared))
